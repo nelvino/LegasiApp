927060223747ae0e25f914d8350330a7536c4dfa
Nico Elvino <nicoelvino@192-168-1-113.tpgi.com.au>
2024-08-28 17:32:38 +1000

    initial components ccreated

---
+++ components/SingleDonationItem/SingleDonationItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Image, View} from 'react-native';
+import PropTypes from 'prop-types';
+
+import Badge from '../Badge/Badge';
+import Header from '../Header/Header';
+
+import style from './style';
+
+const SingleDonationItem = props => {
+  return (
+    <View>
+      <View>
+      <View style={style.badge}>
+          <Badge title={props.badgeTitle} />
+        </View>
+        <Image
+          resizeMode={'contain'}
+          source={{uri: props.uri}}
+          style={style.image}
+        />
+      </View>
+      <View style={style.donationInformation}>
+        <Header title={props.donationTitle} type={3} color={'#0A043C'} />
+        <View style={style.price}>
+          <Header
+            title={'$' + props.price.toFixed(2)}
+            type={3}
+            color={'#156CF7'}
+          />
+        </View>
+      </View>
+      <Header title={props.donationTitle} type={3} color={'#0A043C'} />
+      <Header title={'$' + props.price.toFixed(2)} type={3} color={'#156CF7'} />
+    </View>
+  );
+};
+
+SingleDonationItem.propTypes = {
+  uri: PropTypes.string.isRequired,
+  badgeTitle: PropTypes.string.isRequired,
+  donationTitle: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default SingleDonationItem;
